c8005f398ea2035bc16e7452b26e43ac85e9094b
Sinha Sahab <sinha_sahab@Sinhas-MacBook-Air.local>
2022-02-11 18:21:32 +0530

    replace urlconnection to httpurlconnection, read prev code also

--- src/com/sinha/learn/Main.java
+++ src/com/sinha/learn/Main.java
@@ -3,6 +3,7 @@ package com.sinha.learn;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -16,12 +17,25 @@ public class Main {
             URL url = new URL("http://example.org");
             //below line only creates a connection but does not actually connect. So it just a place where you can
             //config your connection.
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
+            //URLConnection urlConnection = url.openConnection();
+            //urlConnection.setDoOutput(true);
+            //urlConnection.connect();
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent","Chrome");
+            connection.setReadTimeout(30000);
+
+            //below code implicitly establishes the network connection so we dont have to explicitly call the connect
+            //method in this case.
+            int response = connection.getResponseCode();
+            if (response!=200){
+                System.out.println("something wrong with the connection");
+                return;
+            }
 
             //Below code is to fetch the headers being used by the above URL.
-            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+            Map<String, List<String>> headerFields = connection.getHeaderFields();
             for (Map.Entry<String,List<String>> entry: headerFields.entrySet()){
                 String key = entry.getKey();
                 List<String> value = entry.getValue();
@@ -31,7 +45,7 @@ public class Main {
                 }
             }
 
-            BufferedReader input = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line = "";
             while(line!=null){
                 line = input.readLine();
